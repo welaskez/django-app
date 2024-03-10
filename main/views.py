587892be3b0e381dc70fe64282b671b68f509282
Welaskez <nazar.nikitenko.03@mail.ru>
2024-03-10 12:43:25 +0500

    q

---
+++ main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница магазина - HOME',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': False,
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return render(request, 'about.html')
