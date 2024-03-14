283d3b0f26e526a2730114f669b751c1d7e2412a
welaskez <nazarnikitenko53@gmail.com>
2024-03-14 21:48:34 +0500

    Slug converter for categories

--- goods/views.py
+++ goods/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Products
 
 
-def catalog(request):
-    goods = Products.objects.all()
-    
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+
     context = {
         "title": "Home - Каталог",
         "goods": goods,
