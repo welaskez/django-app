b6a14fb2ae753bf6ca0acad52e4f0e647e3d2e40
welaskez <welaskez@hurry.com>
2024-03-18 14:59:38 +0500

    Add to cart ajax request and json response

--- carts/views.py
+++ carts/views.py
@@ -1,16 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
 from goods.models import Products
 
 from carts.models import Cart
+from carts.utils import get_user_carts
 
-def cart_add(request, product_slug):
-    """Берет продукт по его слагу, проверяет авторизован ли юзер, 
-    вытаскивает все товары из корзины конкретного юзера по конкретному продукту,
-    если они есть то берет самую первую, добавляет к её количеству 1 и сохраняет в бд,
-    если её нет то создает новую с количеством 1 и возвращает пользователя на ту же страницу где он был"""
+def cart_add(request):
 
-    product = Products.objects.get(slug=product_slug)
+    product_id = request.POST.get('product_id')
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -23,13 +23,26 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META['HTTP_REFERER'])
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html",
+        {'carts': user_cart},
+        request=request
+    )
+
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
+    }
+
+    return JsonResponse(response_data)
+    
 
 def cart_change(request, product_slug):
     ...
 
 
-def cart_remove(request, cart_id):
+def cart_remove(request):
     cart = Cart.objects.get(id=cart_id)
     cart.delete()
 
